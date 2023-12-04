253403b37848a0fb49e582b78875162750e779db
justI6E <zhakengeradilet003@gmail.com>
2023-12-04 20:22:47 +0600

    lesson5

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -3,45 +3,18 @@ package com.example.myfirstapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    var counter = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        Log.d("MyLog", "onCreate1")
-        Log.d("MyLog", "onCreate2")
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.d("MyLog", "onStart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d("MyLog", "onResume")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d("MyLog", "onPause")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d("MyLog", "onDestroy")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d("MyLog", "onStop")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.d("MyLog", "onRestart")
+    fun onClickTest(view : View) {
+        val tvTest = findViewById<TextView>(R.id.tvTest)
+        tvTest.text = "Hello, favushin!"
     }
 }
