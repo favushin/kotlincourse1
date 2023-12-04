7cc2dc4e1b8f1d449a7a02b07c6c1308371646f6
justI6E <zhakengeradilet003@gmail.com>
2023-12-04 19:13:48 +0600

    lesson4

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     var counter = 0
@@ -9,21 +10,38 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("MyLog", "onCreate1")
+        Log.d("MyLog", "onCreate2")
     }
 
     override fun onStart() {
         super.onStart()
+        Log.d("MyLog", "onStart")
     }
 
     override fun onResume() {
         super.onResume()
+        Log.d("MyLog", "onResume")
     }
 
     override fun onPause() {
         super.onPause()
+        Log.d("MyLog", "onPause")
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        Log.d("MyLog", "onDestroy")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("MyLog", "onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("MyLog", "onRestart")
     }
 }
