5b198cf55eca7f831114fbe95a1fa510b5290562
justI6E <zhakengeradilet003@gmail.com>
2023-12-04 21:07:27 +0600

    lesson6

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -7,14 +7,17 @@ import android.view.View
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    var counter = 5
+    var counter2 = 9
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val tvTest = findViewById<TextView>(R.id.tvTest)
+        var counter3 = counter + counter2
+        tvTest.text = counter3.toString()
     }
 
     fun onClickTest(view : View) {
-        val tvTest = findViewById<TextView>(R.id.tvTest)
-        tvTest.text = "Hello, favushin!"
     }
 }
