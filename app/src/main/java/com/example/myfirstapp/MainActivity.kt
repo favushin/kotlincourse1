c61f662782062e15d4e5876663c9a8bef2632e55
justI6E <zhakengeradilet003@gmail.com>
2023-12-06 18:06:38 +0600

    lesson12(part-2)

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.example.myfirstapp
 
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.TextView
+import com.example.myfirstapp.constances.Constance
 import com.example.myfirstapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -20,21 +17,43 @@ class MainActivity : AppCompatActivity() {
         bindingClass.bMessage.setOnClickListener {
             val resultValue = bindingClass.ptEnter.text.toString()
             when(resultValue) {
-                "Yerlan" -> {
+                Constance.ceo -> {
                     bindingClass.tvMessage.visibility = View.VISIBLE
-                    bindingClass.tvMessage.text = "you are ceo,\n" + "take your 100000$"
+                    val tempText = "you are ceo,\n" + "take your ${Constance.salaryOfCeo}$"
+                    if(bindingClass.ptPassword.text.toString() == Constance.passwordOfCeo) {
+                        bindingClass.tvMessage.text = tempText
+                    }
+                    else {
+                        bindingClass.tvMessage.text = "Uncorrect password!"
+                    }
                 }
-                "Adilet" -> {
+
+                Constance.engineer -> {
                     bindingClass.tvMessage.visibility = View.VISIBLE
-                    bindingClass.tvMessage.text = "you are engineer,\n" + "take your 12000$"
+                    val tempText = "you are engineer,\n" + "take your ${Constance.salaryOfEngineer}$"
+                    if(bindingClass.ptPassword.text.toString() == Constance.passwordOfEngineer) {
+                        bindingClass.tvMessage.text = tempText
+                    }
+                    else {
+                        bindingClass.tvMessage.text = "Uncorrect password!"
+                    }
                 }
-                "Serik" -> {
+
+                Constance.waiter -> {
                     bindingClass.tvMessage.visibility = View.VISIBLE
-                    bindingClass.tvMessage.text = "you are waiter,\n" + "take your 1300$"
+                    val tempText = "you are waiter,\n" + "take your ${Constance.salaryOfWaiter}$"
+                    if(bindingClass.ptPassword.text.toString() == Constance.passwordOfWaiter) {
+                        bindingClass.tvMessage.text = tempText
+                    }
+                    else {
+                        bindingClass.tvMessage.text = "Uncorrect password!"
+                    }
                 }
+
                 else -> {
                     bindingClass.tvMessage.visibility = View.VISIBLE
-                    bindingClass.tvMessage.text = "you don't work in this corporation"
+                    val tempText = "you don't work in this corporation"
+                    bindingClass.tvMessage.text = tempText
                 }
             }
         }
---
+++ app/src/main/java/com/example/myfirstapp/constances/Constance.kt
@@ -0,0 +1,15 @@
+package com.example.myfirstapp.constances
+
+object Constance {
+    const val salaryOfCeo = 100000
+    const val salaryOfEngineer = 12500
+    const val salaryOfWaiter = 1300
+
+    const val ceo = "Yerlan"
+    const val engineer = "Adilet"
+    const val waiter = "Serik"
+
+    const val passwordOfCeo = "4365"
+    const val passwordOfEngineer = "9656"
+    const val passwordOfWaiter = "6513"
+}
