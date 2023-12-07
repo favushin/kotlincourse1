f6246767df23046dcb3dd965b5048f2e6b84c8f3
justI6E <zhakengeradilet003@gmail.com>
2023-12-07 18:34:14 +0600

    lesson13

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import com.example.myfirstapp.constances.Constance
 import com.example.myfirstapp.databinding.ActivityMainBinding
@@ -15,16 +16,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(bindingClass.root)
 
         bindingClass.bMessage.setOnClickListener {
+            Log.d("MyLog", "${R.drawable.engineer}")
             val resultValue = bindingClass.ptEnter.text.toString()
+            bindingClass.ivAvatar.visibility = View.VISIBLE
             when(resultValue) {
                 Constance.ceo -> {
                     bindingClass.tvMessage.visibility = View.VISIBLE
                     val tempText = "you are ceo,\n" + "take your ${Constance.salaryOfCeo}$"
                     if(bindingClass.ptPassword.text.toString() == Constance.passwordOfCeo) {
                         bindingClass.tvMessage.text = tempText
+                        bindingClass.ivAvatar.setImageResource(R.drawable.ceo)
                     }
                     else {
                         bindingClass.tvMessage.text = "Uncorrect password!"
+                        bindingClass.ivAvatar.setImageResource(R.drawable.x)
                     }
                 }
 
@@ -33,9 +38,11 @@ class MainActivity : AppCompatActivity() {
                     val tempText = "you are engineer,\n" + "take your ${Constance.salaryOfEngineer}$"
                     if(bindingClass.ptPassword.text.toString() == Constance.passwordOfEngineer) {
                         bindingClass.tvMessage.text = tempText
+                        bindingClass.ivAvatar.setImageResource(R.drawable.engineer)
                     }
                     else {
                         bindingClass.tvMessage.text = "Uncorrect password!"
+                        bindingClass.ivAvatar.setImageResource(R.drawable.x)
                     }
                 }
 
@@ -44,9 +51,11 @@ class MainActivity : AppCompatActivity() {
                     val tempText = "you are waiter,\n" + "take your ${Constance.salaryOfWaiter}$"
                     if(bindingClass.ptPassword.text.toString() == Constance.passwordOfWaiter) {
                         bindingClass.tvMessage.text = tempText
+                        bindingClass.ivAvatar.setImageResource(R.drawable.waiter)
                     }
                     else {
                         bindingClass.tvMessage.text = "Uncorrect password!"
+                        bindingClass.ivAvatar.setImageResource(R.drawable.x)
                     }
                 }
 
@@ -54,6 +63,7 @@ class MainActivity : AppCompatActivity() {
                     bindingClass.tvMessage.visibility = View.VISIBLE
                     val tempText = "you don't work in this corporation"
                     bindingClass.tvMessage.text = tempText
+                    bindingClass.ivAvatar.setImageResource(R.drawable.x)
                 }
             }
         }
