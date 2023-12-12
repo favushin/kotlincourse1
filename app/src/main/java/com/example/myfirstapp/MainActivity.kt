77e786ad05d113ea3fb2b496731423b596d9bdd3
justI6E <zhakengeradilet003@gmail.com>
2023-12-12 12:34:01 +0600

    lesson19

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,82 +1,36 @@
 package com.example.myfirstapp
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import androidx.core.view.isVisible
-import com.example.myfirstapp.constances.Constance
+import androidx.appcompat.app.AppCompatActivity
 import com.example.myfirstapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var bindingClass : ActivityMainBinding
-    private var login : String = "empty"
-    private var password : String = "empty"
-    private var name : String = "empty"
-    private var surname: String = "empty"
-    private var middleName : String = "empty"
-    private var imageAvatar : Int = 0
+    lateinit var bindingView : ActivityMainBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
-        bindingClass = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(bindingClass.root)
-    }
+        bindingView = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingView.root)
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
+        var array = arrayOf(1, 2, 3)
+        var array2 : Array<Int> = arrayOf(1, 2, 3)
+        var array3 : IntArray = intArrayOf(1, 2, 3)
+        var array4 = intArrayOf(1, 2, 3)
 
-        if(requestCode == Constance.REQUEST_CODE_SIGN_IN) {
-            val l = data?.getStringExtra(Constance.LOGIN)
-            val p = data?.getStringExtra(Constance.PASSWORD)
-            if(login == l && password == p) {
-                bindingClass.ivAvatar.visibility = View.VISIBLE
-                bindingClass.ivAvatar.setImageResource(imageAvatar)
-                val textInfo = "$name $surname $middleName"
-                bindingClass.tvInfo.text = textInfo
-                bindingClass.buttonExit.text = "Exit"
-                bindingClass.buttonGone.visibility = View.GONE
-            }
-            else {
-                bindingClass.ivAvatar.visibility = View.VISIBLE
-                bindingClass.ivAvatar.setImageResource(R.drawable.x)
-                bindingClass.tvInfo.text = "An account does not exist!"
-            }
-        }
-        else if(requestCode == Constance.REQUEST_CODE_SIGN_UP) {
-            login = data?.getStringExtra(Constance.LOGIN)!!
-            password = data.getStringExtra(Constance.PASSWORD)!!
-            name = data.getStringExtra(Constance.NAME)!!
-            surname = data.getStringExtra(Constance.SURNAME)!!
-            middleName = data.getStringExtra(Constance.MIDDLE_NAME)!!
-            imageAvatar = data.getIntExtra(Constance.AVATAR, 0)
-            bindingClass.ivAvatar.visibility = View.VISIBLE
-            bindingClass.ivAvatar.setImageResource(imageAvatar)
-            val textInfo = "$name $surname $middleName"
-            bindingClass.tvInfo.text = textInfo
-            bindingClass.buttonExit.text = "Exit"
-            bindingClass.buttonGone.visibility = View.GONE
-        }
-    }
+        var array5 = IntArray(3)
+        var array6 = arrayOfNulls<Int>(3)
+        var array7 = emptyArray<Int>()
 
-    fun onClickSignIn(view : View) {
-        if(bindingClass.ivAvatar.isVisible && bindingClass.tvInfo.text.toString() != "An account does not exist!") {
-            bindingClass.ivAvatar.visibility = View.INVISIBLE
-            bindingClass.tvInfo.text = ""
-            bindingClass.buttonExit.text = getString(R.string.sign_in)
-            bindingClass.buttonGone.visibility = View.VISIBLE
-        }
-        else {
-            val intent = Intent(this, SignInUpActivity::class.java)
-            intent.putExtra(Constance.SIGN_STATE, Constance.SIGN_IN_STATE)
-            startActivityForResult(intent, Constance.REQUEST_CODE_SIGN_IN)
-        }
-    }
+        array.contentToString()
+        array.reverse()
 
-    fun onClickSignUp(view : View) {
-        val intent = Intent(this, SignInUpActivity::class.java)
-        intent.putExtra(Constance.SIGN_STATE, Constance.SIGN_UP_STATE)
-        startActivityForResult(intent, Constance.REQUEST_CODE_SIGN_UP)
+        var list = listOf(1, 2, 3)
+        var arrayList = arrayListOf(1, 2, 3)
+        arrayList.add(4)
+        arrayList.removeAt(3)
+        arrayList.addAll(list)
+
+        Log.d("MyLog", "output: $arrayList")
     }
 }
