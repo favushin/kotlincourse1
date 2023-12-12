b91867362c9a47d761b0c3131c382b98410dfe46
justI6E <zhakengeradilet003@gmail.com>
2023-12-12 19:15:08 +0600

    lesson23

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -9,11 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         var user = User("Adilet", "54565", 20)
-        var user2 = User("Yermek", "65581", 32)
-
-        user.addAge(10)
-        user2.addAge(5)
-
-        user.printUserInfo()
+        var userInfo = user.addAge()
+        Log.d("MyLog", userInfo)
     }
 }
