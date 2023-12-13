5ec52d3248215c3b24a075c91131b0875b02ccaa
justI6E <zhakengeradilet003@gmail.com>
2023-12-13 22:31:25 +0600

    lesson29

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -5,14 +5,36 @@ import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myfirstapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.title = "favuraki"
+        supportActionBar?.title = "Bottom Navigation Bar"
+
+        binding.bottomMenu.selectedItemId = R.id.draw
+        binding.bottomMenu.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.draw -> {
+                    Toast.makeText(this, "Draw", Toast.LENGTH_SHORT).show()
+                }
+                R.id.attach_file -> {
+                    Toast.makeText(this, "Attach File", Toast.LENGTH_SHORT).show()
+                }
+                R.id.mark -> {
+                    Toast.makeText(this, "Mark", Toast.LENGTH_SHORT).show()
+                }
+                R.id.done -> {
+                    Toast.makeText(this, "Done", Toast.LENGTH_SHORT).show()
+                }
+            }
+            true
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
