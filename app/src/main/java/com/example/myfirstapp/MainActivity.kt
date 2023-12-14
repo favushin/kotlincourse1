66be4c95a09c3e2740c76ff4d9d6c1a5bddc1414
justI6E <zhakengeradilet003@gmail.com>
2023-12-14 12:58:16 +0600

    lesson30

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.myfirstapp
 
 import android.os.Bundle
+import android.view.Gravity
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import com.example.myfirstapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,53 +16,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.title = "Bottom Navigation Bar"
-
-        binding.bottomMenu.selectedItemId = R.id.draw
-        binding.bottomMenu.setOnItemSelectedListener {
-            when(it.itemId) {
-                R.id.draw -> {
-                    Toast.makeText(this, "Draw", Toast.LENGTH_SHORT).show()
-                }
-                R.id.attach_file -> {
-                    Toast.makeText(this, "Attach File", Toast.LENGTH_SHORT).show()
-                }
-                R.id.mark -> {
-                    Toast.makeText(this, "Mark", Toast.LENGTH_SHORT).show()
-                }
-                R.id.done -> {
-                    Toast.makeText(this, "Done", Toast.LENGTH_SHORT).show()
-                }
-            }
-            true
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.main_menu, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId) {
-            android.R.id.home -> finish()
-            R.id.search -> {
-                Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show()
-            }
-
-            R.id.sync -> {
-                Toast.makeText(this, "Sync", Toast.LENGTH_SHORT).show()
-            }
-
-            R.id.save -> {
-                Toast.makeText(this, "Save", Toast.LENGTH_SHORT).show()
-            }
-
-            R.id.delete -> {
-                Toast.makeText(this, "Delete", Toast.LENGTH_SHORT).show()
+        binding.apply {
+            button.setOnClickListener {
+                drawer.openDrawer(GravityCompat.START)
             }
         }
-        return true
     }
 }
