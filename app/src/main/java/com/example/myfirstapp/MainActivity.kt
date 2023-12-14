450048ff8ac5d9b8c8b59d1c8de32bbc9b7bf65e
justI6E <zhakengeradilet003@gmail.com>
2023-12-14 19:25:14 +0600

    countdown timer

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myfirstapp
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -8,22 +9,29 @@ import com.example.myfirstapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    private var timer: CountDownTimer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.tvReto.setOnClickListener {
-            binding.tvReto.text = getReto()
+        binding.apply {
+            buttonStart.setOnClickListener {
+                startCountDownTimer(20000)
+            }
         }
     }
 
-    private fun getReto(): String {
-        return resources.getStringArray(R.array.retos)[randomNumber()]
-    }
+    private fun startCountDownTimer(timeMillis: Long) {
+        timer?.cancel()
+        timer = object : CountDownTimer(timeMillis, 1000) {
+            override fun onTick(timeM: Long) {
+                binding.tvTimer.text = timeM.toString()
+            }
 
-    private fun randomNumber(): Int {
-        val size = resources.getStringArray(R.array.retos).size - 1
-        return (0..size).random()
+            override fun onFinish() {
+                binding.tvTimer.text = "Finish"
+            }
+        }.start()
     }
 }
